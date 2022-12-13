53a603f8f5e43410dd4dd595ac3a31c0660e004d
XeNoXTS <palmyunvisate2003@gmail.com>
2022-12-13 15:42:21 +0700

    lan7_4 Completed

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -6,10 +6,21 @@ using namespace std;
 int main(){
 	
 	string name[] = {"Luffy","Zoro","Sanji","Nami","Naruto","Sakura","All Might","Saitama","Eren","Mikasa"};
-	int id[] = {123,196,225,329,420,451,515,666,690,787};
-	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
+	int i=0, id[] = {123,196,225,329,420,451,515,666,690,787};
+	double g, gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+	int L= sizeof(name)/sizeof(string);
+
+    cout << "Please input GPA: ";
+	cin >> g;
+	while(i < L)
+	{
+		if(gpa[i] >= g)
+		{
+			cout << id[i] << ' ' << name[i] << ' ' << gpa[i] << endl;
+		}
+		i++;
+	}
 		
 	return 0;
 }
