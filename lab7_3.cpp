fcf8b5668e2325d32d1978b2db6952d6b82206a1
XeNoXTS <palmyunvisate2003@gmail.com>
2022-12-13 16:02:46 +0700

    lab7_3 Completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,34 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
+int adiff(int a, int b)
+{
+  a = a%360;
+  b = b%360;
+  int c = a - b;
+  if(c > 180)
+  {
+    c = 360 - c;
+  }
+  if(c < -180)
+  {
+    c = c + 360;
+  }
+  if(c < 0)
+  {
+    c = -c;
+  }
+  return c;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << endl;
+  cout << adiff(210,45) << endl;
+  cout << adiff(0,360) << endl;
+  cout << adiff(10,350) << endl;
+  cout << adiff(95,260) << endl;
+  cout << adiff(90,-90) << endl;
+  cout << adiff(1000,280) << endl;
+  cout << adiff(60,244) << endl;
 }
